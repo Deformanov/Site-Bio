90fbe4e629f53221ab5c40beec6861c977819c25
Deformanov <korzikov1sasa@gmail.com>
2025-08-02 12:47:59 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+    function toggleTheme() {
+      const body = document.body;
+      const icon = document.getElementById('theme-icon');
+      const isDark = body.classList.toggle('dark-theme');
+      icon.className = isDark ? 'fas fa-sun' : 'fas fa-moon';
+    }
+  
